94fb97b0f637b712d71933ef9f66c4c4bb56a9bf
Skooling80 <skooling@gmail.com>
2024-06-13 01:47:46 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('show');
+    });
+});
